dbd7a63cb3df9dfcc0d97eccf041ee852c72a69d
Hager Abdelmotaal <hager.abdelmotaal@espace.com.eg>
2012-09-16 18:05:27 +0200

    change the routes to be compatible with rails 3 and redmine version 2.0.3

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
-ActionController::Routing::Routes.draw do |map|
-  map.connect 'queries/track.:format',:controller=>"queries_engine",:action=>"track"
-  map.connect 'queries/track_all.:format',:controller=>"queries_engine",:action=>"get_all_time_entries_from_begining_of_month_till_now"
-  map.connect 'queries/average_logged_hours_till_now.:format',:controller=>"queries_engine",:action=>"average_logged_hours_till_now"
-  map.connect 'queries/get_activities.:format',:controller=>"queries_engine",:action=>"get_all_activities"
-  map.connect 'queries/get_charts_data.:format',:controller=>"queries_engine",:action=>"get_charts_data"
+if Rails::VERSION::MAJOR >= 3
+  RedmineApp::Application.routes.draw do
+    match 'queries/track.:format', :controller => "queries_engine" , :action => "track"
+    match 'queries/track_all.:format', :controller => "queries_engine", :action => "get_all_time_entries_from_begining_of_month_till_now"
+    match 'queries/average_logged_hours_till_now.:format', :controller => "queries_engine" , :action => "average_logged_hours_till_now"
+    match 'queries/get_activities.:format', :controller => "queries_engine" , :action => "get_all_activities"
+    match 'queries/get_charts_data.:format', :controller => "queries_engine" , :action => "get_charts_data"
+  end
+else
+  ActionController::Routing::Routes.draw do |map|
+    map.connect 'queries/track.:format',:controller=>"queries_engine",:action=>"track"
+    map.connect 'queries/track_all.:format',:controller=>"queries_engine",:action=>"get_all_time_entries_from_begining_of_month_till_now"
+    map.connect 'queries/average_logged_hours_till_now.:format',:controller=>"queries_engine",:action=>"average_logged_hours_till_now"
+    map.connect 'queries/get_activities.:format',:controller=>"queries_engine",:action=>"get_all_activities"
+    map.connect 'queries/get_charts_data.:format',:controller=>"queries_engine",:action=>"get_charts_data"
+ end
 end
+
