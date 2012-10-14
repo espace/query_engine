eeae8cfd215bf06e5defd59a145c624131817f24
Yasmine Gaber <yasmine.gaber@espace.com.eg>
2012-10-14 16:50:10 +0200

    upgrade to redmine 2.1

--- init.rb
+++ init.rb
@@ -1,2 +1,10 @@
 # Include hook code here
-require 'query_engine'
+require 'redmine'
+require_dependency 'query_engine'
+
+Redmine::Plugin.register :query_engine do
+  name 'Redmine Query Engine'
+  author 'Yasmine Gaber'
+  description 'It exports logged time on redmine to eTime'
+  version '1.0.0'
+end
